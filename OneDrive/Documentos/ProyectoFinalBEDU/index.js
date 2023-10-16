e43954f33af0f1e5ce62c3b4c929768fa78e9d0d
AleidyTest <aleidyedith.rincon@upaep.mx>
2023-10-15 23:42:43 -0600

    Sequalize con consulta

--- OneDrive/Documentos/ProyectoFinalBEDU/index.js
+++ OneDrive/Documentos/ProyectoFinalBEDU/index.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require("sequelize");
+const { Sequelize, DataTypes } = require("sequelize");
 
 
 // Conexión con MySQL
@@ -18,6 +18,63 @@ const sequelize = new Sequelize({
 	// El nombre de la base de datos
 	database: "todolist",
 });
+
+//modelo
+const Tarea = sequelize.define("tareas", {
+	idTarea: {
+	  type: DataTypes.INTEGER,
+	  primaryKey: true,
+	  autoIncrement: true,
+	},
+	idUsuario: {
+	  type: DataTypes.INTEGER,
+	},
+	titulo: {
+	  type: DataTypes.STRING(255),
+	},
+	descripcion: {
+	  type: DataTypes.STRING(255),
+	},
+	fechaCreacion: {
+	  type: DataTypes.DATE,
+	},
+	fechalimite: {
+	  type: DataTypes.DATEONLY,
+	},
+	estatus: {
+	  type: DataTypes.ENUM("pendiente", "completada", "en progreso"),
+	},
+	prioridad: {
+	  type: DataTypes.ENUM("alta", "media", "baja"),
+	},
+  },
+  {
+	// Indica que Sequelize no debe agregar createdAt y updatedAt
+	timestamps: false,
+  });
+
+  //Creas tarea
+  Tarea.create({
+	idUsuario: 1,
+	titulo: "Tarea de ejemplo",
+	descripcion: "Esta es una tarea de ejemplo",
+	fechaCreacion: new Date(),
+	fechalimite: "2023-12-31",
+	estatus: "pendiente",
+	prioridad: "alta",
+  });
+//consultas tarea
+async function consultarTareas() {
+	try {
+	  const todasLasTareas = await Tarea.findAll();
+	  console.log(todasLasTareas);
+	} catch (error) {
+	  console.error("Error al consultar tareas:", error);
+	}
+  }
+  
+  consultarTareas();
+
 
 async function connect() {
 	try {
